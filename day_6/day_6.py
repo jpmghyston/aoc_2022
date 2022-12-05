3752555ab42c7a24d94548567c9f9185e3ac2d75
Jake Mckenna <jake.mckenna@ghyston.com>
2022-12-05 21:26:09 +0000

    Template file for day 6

---
+++ day_6/day_6.py
@@ -0,0 +1,13 @@
+def get_input():
+    with open("day6_input.txt") as f:
+        return [x.strip() for x in f.readlines()]
+
+def part_1():
+    pass
+
+def part_2():
+    pass
+
+if __name__ == "__main__":
+    part_1()
+    part_2()
