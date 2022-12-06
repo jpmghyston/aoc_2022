d434debef6346c1b382f9678ce106606962ed24a
Jake McKenna <Jake.McKenna@Ghyston.com>
2022-12-06 08:06:05 +0000

    Day 6 done

--- day_6/day_6.py
+++ day_6/day_6.py
@@ -1,12 +1,32 @@
 def get_input():
+    # return "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
     with open("day6_input.txt") as f:
-        return [x.strip() for x in f.readlines()]
+        return f.read().strip()
 
 def part_1():
-    pass
+    datastream = get_input()
+    for i in range(4, len(datastream)):
+        last_four_characters = datastream[i-4:i]
+        if not contains_repeated_char(last_four_characters):
+            print(i)
+            return
+
 
 def part_2():
-    pass
+    datastream = get_input()
+    for i in range(14, len(datastream)):
+        last_four_characters = datastream[i-14:i]
+        if not contains_repeated_char(last_four_characters):
+            print(i)
+            return
+
+
+def contains_repeated_char(string):
+    for char in string:
+        if string.count(char) > 1:
+            return True
+    return False
+
 
 if __name__ == "__main__":
     part_1()
