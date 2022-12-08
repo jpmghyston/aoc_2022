20b8ab03a68245116c580abba72b00185a04b334
Jake McKenna <Jake.McKenna@Ghyston.com>
2022-12-08 13:37:21 +0000

    Refactor to use generators

--- day_8/day_8.py
+++ day_8/day_8.py
@@ -47,21 +47,11 @@ with open("day8_input.txt") as f:
 height = len(grid)
 width = len(grid[0])
 
-visible_count = (height * 2 + width * 2) - 4
+outer_visible_trees = (height * 2 + width * 2) - 4
 
-max_scenic_score = 0
+inner_visible_trees = sum((1 for i in range(1, height - 1) for j in range(1, width - 1) if tree_is_visible(grid, i, j)))
 
-for i in range(1, height - 1):
-    for j in range(1, width - 1):
-        # Part 1
-        if tree_is_visible(grid, i, j):
-            visible_count += 1
+max_scenic_score = max(tree_scenic_score(grid, i, j) for i in range(1, height - 1) for j in range(1, width - 1))
 
-        # Part 2
-        tree_score = tree_scenic_score(grid, i, j)
-        if max_scenic_score < tree_score:
-            max_scenic_score = tree_score
-
-
-print(visible_count)
+print(outer_visible_trees + inner_visible_trees)
 print(max_scenic_score)
