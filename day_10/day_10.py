65e18862799999337d4f9d1e46de283a367cd28e
Jake Mckenna <jake.mckenna@ghyston.com>
2022-12-10 12:58:59 +0000

    Day 10

---
+++ day_10/day_10.py
@@ -0,0 +1,36 @@
+with open("day_10/day10_input.txt") as f:
+    instructions =list(reversed([x.strip().split(" ") for x in f.readlines()]))
+
+register = 1
+addNextCycle = []
+
+screen = [["." for _ in range(40)] for _ in range(6)]
+
+def print_screen(screen):
+    for line in screen:
+        print("".join(line))
+        
+
+relevantCycles = set([20, 60, 100, 140, 180, 220])
+signalStrengths = []
+cycle = 0
+while len(instructions) > 1:
+    pixel_x = cycle % 40
+    pixel_y = cycle // 40
+    if register - 1 <= pixel_x <= register + 1:
+        screen[pixel_y][pixel_x] = "#"
+    cycle += 1
+    if cycle in relevantCycles:
+        signalStrengths.append(cycle * register)
+    if len(addNextCycle) > 0:
+        register += addNextCycle.pop()
+    else:
+        match instructions.pop():
+            case ["noop"]:
+                pass
+            case ["addx", val]:
+                addNextCycle.append(int(val))
+
+
+print(sum(signalStrengths))
+print_screen(screen)
