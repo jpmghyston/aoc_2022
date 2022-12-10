5918fb19fa384f305bbbc87edbebfe887576508c
Jake Mckenna <jake.mckenna@ghyston.com>
2022-12-10 13:05:09 +0000

    Reformat a bit

--- day_10/day_10.py
+++ day_10/day_10.py
@@ -1,19 +1,20 @@
+def print_screen(screen):
+    for line in screen:
+        print("".join(line))
+
+
 with open("day_10/day10_input.txt") as f:
-    instructions =list(reversed([x.strip().split(" ") for x in f.readlines()]))
+    instructions = list(reversed([x.strip().split(" ") for x in f.readlines()]))
 
 register = 1
 addNextCycle = []
 
 screen = [["." for _ in range(40)] for _ in range(6)]
-
-def print_screen(screen):
-    for line in screen:
-        print("".join(line))
         
-
 relevantCycles = set([20, 60, 100, 140, 180, 220])
 signalStrengths = []
 cycle = 0
+
 while len(instructions) > 1:
     pixel_x = cycle % 40
     pixel_y = cycle // 40
